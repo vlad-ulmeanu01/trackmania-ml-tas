b9b3e6926858b79253ebdce41bb44a75b76c90a3
vlad-ulmeanu01 <vlad.ulmeanu01@gmail.com>
2021-08-03 09:14:03 +0300

    Squashed commit of the following:
    commit 281e7c9da07aa292a3a01a58ca644438747f5a61
    Author: vlad-ulmeanu01 <vlad.ulmeanu01@gmail.com>
    Date:   Tue Aug 3 09:13:22 2021 +0300
    
        mlmain strat A
    
        merge mlmain, nu prea gaseste imbunatatiri)) mai face alte run-uri de 23 72, dar sunt aproape identice
    
    commit 1dbf9ff2a7b953c0ea95e15f830bf997dedb856a
    Author: vlad-ulmeanu01 <vlad.ulmeanu01@gmail.com>
    Date:   Mon Aug 2 19:25:29 2021 +0300
    
        Update mlmain.py
    
        refix + fixat si alte buguri
    
    commit 5bfe46fdab232e1390ea45dc41ea938354860804
    Author: vlad-ulmeanu01 <vlad.ulmeanu01@gmail.com>
    Date:   Mon Aug 2 18:51:44 2021 +0300
    
        Update mlmain.py
    
        speram ca e fixat bugul? nu am testat deocamdata
    
    commit 049ac46ed6f2015c21373e4ad19ffba8ed3d8e64
    Author: vlad-ulmeanu01 <73878428+vlad-ulmeanu01@users.noreply.github.com>
    Date:   Mon Aug 2 14:56:54 2021 +0300
    
        Update mlmain.py
    
        mai e cel putin un bug:
        File "C:\Users\Matei\Desktop\trackmania-ml-tas-needs-testing\TMInterfaceClientPython-master\mlmain.py", line 226, in combine_inputs
            assert(len(percentages) == len(inputs))
        AssertionError
    
        cred ca am busit ceva la combine_inputs
    
    commit 61166068ef2e7f60599403c2e059778c972bc026
    Author: vlad-ulmeanu01 <vlad.ulmeanu01@gmail.com>
    Date:   Mon Aug 2 14:22:18 2021 +0300
    
        ml code
    
        needs testing!!
        also fixed 23.73 to 23.72, didn't parse some weird press operations

--- TMInterfaceClientPython-master/mlmain.py
+++ TMInterfaceClientPython-master/mlmain.py
@@ -3,6 +3,7 @@ from tminterface.client import Client
 import sys
 import signal
 import time
+import copy
 
 def read_processed_input(f: str):
     fin = open(f)
@@ -48,20 +49,21 @@ def write_processed_output(g, sol, GAP_TIME):
 class MainClient(Client):
     def __init__(self):
         self.GAP_TIME, self.CUTOFF_TIME = 10, 25000 #ms
-        self.LEFT_SHIFTS, self.RIGHT_SHIFTS = 7, 7
         self.IND_STEER, self.IND_PUSH_UP, self.IND_PUSH_DOWN = 0, 1, 2
-        
-        self.processed_input_dir = "../tm_data_reader/Processed-inputs/input_"
-        #presupunand ca fisierul din care se ruleaza e TMInterfaceClientPython-master/
+
         self.processed_output_dir = "Processed-outputs/output_"
         
         self.last_time_in_run_step = 0
         self.time_to_remember_state, self.remembered_state = (-1100, -500), None
         self.did_race_finish = False
 
-        self.input_arrays = {}
-        for i in range(-self.LEFT_SHIFTS, self.RIGHT_SHIFTS + 1):
-            self.input_arrays[i] = read_processed_input(self.processed_input_dir + str(i) + ".txt")
+        #acest client este un fel de cutie neagra care trebuie sa primeasca niste inputuri, sa le ruleze
+        #si sa returneze scorurile pentru inputuri
+        #cand nu are nimic de rulat (i.e. self.should_client_work == False), clientul nu face nimic
+        #cand primeste >= 1 inputuri, da reset la cursa, calculeaza pana le face pe toate etc
+        self.should_client_work = False #variabila care indica ralanti pentru simulari
+        self.input_stack = [] #stiva cu [inputuri ce se doresc a fi rulate, scorul inputului dupa rulare]
+        self.input_stack_index = -1 #indica care input trebuie rulat acum.
 
         pass
 
@@ -75,10 +77,17 @@ class MainClient(Client):
         pass
 
     def on_run_step(self, iface: TMInterface, time: int):
+        if not self.should_client_work:
+            return
+
         self.last_time_in_run_step = time
 
-        if self.did_race_finish:
+        if self.did_race_finish or time >= self.CUTOFF_TIME:
             self.did_race_finish = False
+
+            if time >= self.CUTOFF_TIME:
+                self.process_input_stack(iface)
+
             iface.rewind_to_state(self.remembered_state)
             return
 
@@ -95,9 +104,11 @@ class MainClient(Client):
         if prepared_act_index >= self.CUTOFF_TIME // self.GAP_TIME:
             return
 
-        iface.set_input_state(up = self.input_arrays[0][self.IND_PUSH_UP][prepared_act_index],
-                              down = self.input_arrays[0][self.IND_PUSH_DOWN][prepared_act_index],
-                              steer = self.input_arrays[0][self.IND_STEER][prepared_act_index])
+
+        iface.set_input_state(up = self.input_stack[self.input_stack_index][0][self.IND_PUSH_UP][prepared_act_index],
+                              down = self.input_stack[self.input_stack_index][0][self.IND_PUSH_DOWN][prepared_act_index],
+                              steer = self.input_stack[self.input_stack_index][0][self.IND_STEER][prepared_act_index])
+
         pass
 
     def on_simulation_begin(self, iface):
@@ -117,17 +128,207 @@ class MainClient(Client):
         iface.prevent_simulation_finish()
 
         oname = self.processed_output_dir + str(int(time.time())) + "_" + str(self.last_time_in_run_step) + ".txt"
-        write_processed_output(oname, self.input_arrays[0], self.GAP_TIME)
+        write_processed_output(oname, self.input_stack[self.input_stack_index][0], self.GAP_TIME)
 
         print("wrote to " + oname + "!")
 
-        #deocamdata e self.input_arrays[0] ca sa vad ca merge inapoi
-        #cu ce dau eu playback am 23s73, cu tminterface ar trebui sa fie 23s72
+        self.process_input_stack(iface)
 
         pass
 
     def on_laps_count_changed(self, iface, current: int):
         pass
+
+    def fitness_function (self, iface):
+        state = iface.get_simulation_state()
+        speed = state.get_display_speed()
+
+        score = 0
+        score += 250 * (23720 - self.last_time_in_run_step)
+        score += speed
+
+        return score
+
+    def add_input_array_to_stack(self, input):
+        self.input_stack.append([copy.deepcopy(input), None])
+        self.input_stack_index += 1
+        if not self.should_client_work:
+            self.should_client_work = True
+
+    #se apeleaza la sfarsitul unei simulari
+    def process_input_stack (self, iface):
+        self.input_stack[self.input_stack_index][1] = self.fitness_function(iface)
+        self.input_stack_index -= 1
+        if self.input_stack_index < 0:
+            self.should_client_work = False
+
+    def empty_stack (self):
+        self.input_stack = []
+        self.input_stack_index = -1
+        pass
+
+
+class ML():
+    def __init__(self):
+        self.GAP_TIME, self.CUTOFF_TIME = 10, 25000
+        self.LEFT_SHIFTS, self.RIGHT_SHIFTS = 7, 7
+        self.IND_STEER, self.IND_PUSH_UP, self.IND_PUSH_DOWN = 0, 1, 2
+
+        self.processed_input_dir = "../tm_data_reader/Processed-inputs/input_"
+        #presupunand ca fisierul din care se ruleaza e TMInterfaceClientPython-master/
+
+        self.input_arrays = [[] for _ in range(self.LEFT_SHIFTS + self.RIGHT_SHIFTS + 1)]
+        for i in range(-self.LEFT_SHIFTS, self.RIGHT_SHIFTS + 1):
+            self.input_arrays[i + self.LEFT_SHIFTS] = read_processed_input(self.processed_input_dir + str(i) + ".txt")
+
+        self.have_current_run = False
+        self.current_run_score = 0
+
+        #pentru fiecare interval [l, r] ai o combinatie de coeficienti
+        #self.intervals[i][0] = (l, r)
+        #self.intervals[i][1] = un vector cu coeficientii corespunzatori de lungime
+        #self.LEFT_SHIFTS + self.RIGHT_SHIFTS + 1
+        #TODO fa posibil splitul unui interval in 2 cu o probabilitate aleatoare
+
+        self.intervals = self.make_intervals(50)
+        self.interval_bounds = (28, 39) #se lucreaza pe intervalele [.., ..)
+        self.curr_itv = self.interval_bounds[0] #indexul intervalului pe care se lucreaza momentan
+        self.percentage_increase = 0.05 #dc procentajul este 0.4 se intra in calcul cu el 0.41
+        self.kept_change = 0.15 #cat din schimbare chiar este facuta
+        self.changed_percentages = [] #tine minte procentele schimbate bagate in stiva, folosite mai
+        #tarziu la calculul gradientilor
+
+        self.epoch_count = 1
+
+        pass
+
+    def make_intervals(self, coef):
+        itv_starts = [x for x in range(0, self.CUTOFF_TIME // self.GAP_TIME, coef)]
+        intervals = []
+        for i in range(len(itv_starts) - 1):
+            intervals.append([(itv_starts[i], itv_starts[i+1] - 1),
+                             [0.0] * self.LEFT_SHIFTS + [1.0] + [0.0] * self.RIGHT_SHIFTS])
+        return intervals
+
+    def normalize_percentages(self, percentages):
+        assert(len(percentages) == self.LEFT_SHIFTS + self.RIGHT_SHIFTS + 1)
+        for i in range(len(percentages)):
+            percentages[i] = max(0, percentages[i])
+        x = sum(percentages)
+        assert(x != 0.0)
+        x = 1 / x
+        for i in range(len(percentages)):
+            percentages[i] *= x
+        return percentages
+
+    #inputs este un vector gen [[steer -> [], push_up -> [], push_down -> []], ... ]
+    #len(inputs) == LEFT_SHIFTS + RIGHT_SHIFTS + 1
+    #len(percentages) == in cate itv ai vrut tu sa spargi linia de timp
+    #percentages[k] = un vector cu LEFT_SHIFTS + RIGHT_SHIFTS + 1 procente
+    #de obicei se apeleaza pentru reuniune de intervale
+    def combine_inputs(self, inputs, percentages):
+        n = len(inputs[0][self.IND_STEER])
+        sol = [[0] * n, [0] * n, [0] * n]
+        for i in range(len(inputs)):
+            for j in range(3):
+                assert(n == len(inputs[i][j]))
+                k = 0
+                for z in range(n):
+                    while k + 1 < len(percentages) and self.intervals[k][0][1] < z:
+                        k += 1
+                    sol[j][z] += percentages[k][i] * inputs[i][j][z]
+
+        for z in range(n):
+            sol[self.IND_STEER][z] = max(-65536, min(65536, int(sol[self.IND_STEER][z])))
+
+        for j in (self.IND_PUSH_UP, self.IND_PUSH_DOWN):
+            for z in range(n):
+                sol[j][z] = round(sol[j][z])
+                assert(sol[j][z] in (0, 1))
+
+        return sol
+
+    def main_loop(self, client: MainClient):
+        if not self.have_current_run:
+            if len(client.input_stack) == 0: #trebuie dat la client inputul actual
+                #client.add_input_array_to_stack(self.input_arrays[0 + self.LEFT_SHIFTS])
+                #trb sa bagi si aici self.combine_inputs VV
+                percentages = []
+                for j in range(len(self.intervals)):
+                    percentages.append(copy.deepcopy(self.intervals[j][1]))
+
+                client.add_input_array_to_stack(self.combine_inputs(self.input_arrays, percentages))
+                pass
+            elif client.input_stack[0][1] != None: #a venit rezultatul
+                self.current_run_score = client.input_stack[0][1]
+                self.have_current_run = True
+                client.empty_stack()
+            return
+
+        if not client.should_client_work:
+            if len(client.input_stack) == 0:
+                #trebuie dat de munca clientului.
+                #trebuie calculate noile procentaje si bagate toate inputurile noi in acelasi timp
+                #in client.input_stack
+
+                #procentajele ce trebuie manipulate sunt self.intervals[self.curr_itv][1]
+                self.changed_percentages = []
+                for i in range(-self.LEFT_SHIFTS, self.RIGHT_SHIFTS + 1):
+                    local_perc = copy.deepcopy(self.intervals[self.curr_itv][1])
+                    local_perc[i + self.LEFT_SHIFTS] += self.percentage_increase
+                    local_perc = self.normalize_percentages(local_perc)
+                    
+                    self.changed_percentages.append(local_perc[i + self.LEFT_SHIFTS]) #folosit in else
+
+                    percentages = []
+                    for j in range(self.curr_itv):
+                        percentages.append(copy.deepcopy(self.intervals[j][1]))
+                    percentages.append(local_perc)
+                    for j in range(self.curr_itv + 1, len(self.intervals)):
+                        percentages.append(copy.deepcopy(self.intervals[j][1]))
+
+                    client.add_input_array_to_stack(self.combine_inputs(self.input_arrays, percentages))
+            else:
+                #daca sunt aici inseamna ca clientul a terminat munca pe care i-am dat-o, trebuie analizata,
+                #trebuie calculate procentajele noi si trebuie golita stiva
+                scores = []
+                for i in range(-self.LEFT_SHIFTS, self.RIGHT_SHIFTS + 1):
+                    scores.append(client.input_stack[i + self.LEFT_SHIFTS][1])
+
+                local_perc = copy.deepcopy(self.intervals[self.curr_itv][1])
+                gradients = []
+                assert(len(self.changed_percentages) == self.LEFT_SHIFTS + self.RIGHT_SHIFTS + 1)
+
+                for i in range(-self.LEFT_SHIFTS, self.RIGHT_SHIFTS + 1):
+                    diff = abs(local_perc[i + self.LEFT_SHIFTS] - self.changed_percentages[i + self.LEFT_SHIFTS])
+                    if diff == 0:
+                        gradients.append(0.0)
+                    else:
+                        gradients.append((scores[i + self.LEFT_SHIFTS] - self.current_run_score) / diff)
+                    gradients[-1] = max(-self.percentage_increase, min(self.percentage_increase, gradients[-1]))
+
+                for i in range(-self.LEFT_SHIFTS, self.RIGHT_SHIFTS):
+                    local_perc[i + self.LEFT_SHIFTS] += self.kept_change * gradients[i + self.LEFT_SHIFTS]
+
+                local_perc = self.normalize_percentages(local_perc)
+                self.intervals[self.curr_itv][1] = copy.deepcopy(local_perc)
+                #!!! doar procentajele trebuie schimbate la sfarsitul reprizei
+                #input_arrays ramane LA FEL intotdeauna
+
+                print(f"Finished epoch no. {self.epoch_count} on interval {self.curr_itv}!")
+
+                client.empty_stack()
+                self.have_current_run = False #trebuie sa recalculez scorul curent
+                self.epoch_count += 1
+                self.curr_itv = self.curr_itv+1 if self.curr_itv+1 < self.interval_bounds[1] else self.interval_bounds[0]
+
+                pass
+        else:
+            #lasa clientul sa lucreze ce i-ai dat.
+            pass
+
+        pass
+
 
 def main():
     server_name = 'TMInterface0'
@@ -147,7 +348,9 @@ def main():
     client = MainClient()
     iface.register(client)
 
+    ml = ML()
     while iface.running:
+        ml.main_loop(client)
         time.sleep(0)
 
 if __name__ == '__main__':
